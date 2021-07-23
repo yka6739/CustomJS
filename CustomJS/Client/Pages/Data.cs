082c3cc3a4d3c945a77f0ac7aec9663241fa12d4
YATENDRA AWANA <yaten96@live.com>
2021-07-23 14:48:36 +0530

    Add project files.

---
+++ CustomJS/Client/Pages/Data.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomJS.Client.Pages
+{
+    public  static class Data
+    {
+        public static async ValueTask MyFunction(this IJSRuntime js, string message)
+        {
+            await js.InvokeVoidAsync("my_function", message);
+        }
+    }
+}
---
+++ CustomJS/Client/wwwroot/js/JavaScript.js
@@ -0,0 +1,3 @@
+﻿function my_function(message) {
+    console.log("From Java Script Function" + message);
+}
